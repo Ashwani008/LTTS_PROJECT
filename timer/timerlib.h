3ba30d9e42fa212d68f12f14031d5f3e91ea7b15
Ashwani008 <55152445+Ashwani008@users.noreply.github.com>
2021-07-13 08:21:59 +0530

    Add files via upload

---
+++ timer/timerlib.h
@@ -0,0 +1,43 @@
+#ifndef __TIMER_WRAP__
+#define __TIMER_WRAP__
+
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef enum{
+
+	TIMER_INIT,
+	TIMER_DELETED,
+	TIMER_PAUSED,
+	TIMER_CANCELLED,
+	TIMER_RESUMED,
+	TIMER_RUNNING,
+} TIMER_STATE_T;
+
+typedef struct Timer_{
+
+	/* Timer config */
+    timer_t *posix_timer;
+    void *user_arg;
+	unsigned long exp_timer;		/* in milli-sec */
+	unsigned long sec_exp_timer;	/* in milli-sec */
+	uint32_t thresdhold;			/* No of times to invoke the timer callback */
+	void (*cb)(struct Timer_ *, void *); /* Timer Callback  */
+	bool exponential_backoff;
+
+	/* place holder value to store
+ 	 * dynamic attributes of timer */
+	unsigned long time_remaining;	/* Time left for paused timer for next expiration */
+	uint32_t invocation_counter; 
+	struct itimerspec ts;
+	unsigned long exp_back_off_time;
+	TIMER_STATE_T timer_state;
+} Timer_t;
+
+
+
+#endif /* __TIMER_WRAP__  */
